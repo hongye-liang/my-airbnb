0462f2de2c00aa70679daa7445e6353f44bcf3ae
Hongye <hongyeliang0206@gmail.com>
2025-09-27 23:29:37 -0700

    making property dynamic and more features to interact

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -1,10 +1,11 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-
-from .models import Property
+from rest_framework_simplejwt.tokens import AccessToken
+from .models import Property, Reservation
 from .forms import PropertyForm
-from .serializers import PropertiesListSerializer, PropertiesDetailSerializer
+from .serializers import PropertiesListSerializer, PropertiesDetailSerializer, ReservationsListSerializer
+from useraccount.models import User
 
 #new
 from rest_framework.permissions import IsAuthenticated 
@@ -14,11 +15,35 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 @authentication_classes([])
 @permission_classes([])
 def properties_list(request):
+    #auth
+    try:
+        token = request.META['HTTP_AUTHORIZATION'].split('Bearer ')[1]
+        token = AccessToken(token)
+        user_id = token.payload['user_id']
+        user = User.objects.get(pk=user_id)
+    except Exception as e:
+        user = None
+
+    print('user,', user)
+
+    favorites = []
     properties = Property.objects.all()
+
+    landlord_id = request.GET.get('landlord_id', '')
+    if landlord_id:
+        properties = properties.filter(landlord_id = landlord_id)
+
+    #favorites
+    if user:
+        for property in properties:
+            if user in property.favorited.all():
+                favorites.append(property.id)
+
     serializer = PropertiesListSerializer(properties, many=True)
 
     return JsonResponse({
-        'data': serializer.data
+        'data': serializer.data,
+        'favorites': favorites
     })
 
 
@@ -31,6 +56,18 @@ def properties_detail(request, pk):
     serializer = PropertiesDetailSerializer(property, many=False)
 
     return JsonResponse(serializer.data)
+
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def property_reservations(request, pk):
+    property = Property.objects.get(pk=pk)
+    reservations = property.reservations.all()
+
+    serializer = ReservationsListSerializer(reservations, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
 
 
 # @api_view(['POST', 'FILES'])
@@ -49,3 +86,47 @@ def create_property(request):
     else:
         print('error', form.errors, form.non_field_errors)
         return JsonResponse({'errors': form.errors.as_json()}, status=400)
+    
+@api_view(['POST'])
+@authentication_classes([JWTAuthentication])      # ensure JWT parsing
+@permission_classes([IsAuthenticated]) 
+def book_property(request, pk):
+    try:
+        start_date = request.POST.get('start_date', '')
+        end_date = request.POST.get('end_date', '')
+        number_of_nights = request.POST.get('number_of_nights', '')
+        total_price = request.POST.get('total_price', '')
+        guests = request.POST.get('guests', '')
+
+        property = Property.objects.get(pk=pk)
+
+        Reservation.objects.create(
+            property=property,
+            start_date=start_date,
+            end_date=end_date,
+            number_of_nights=number_of_nights,
+            total_price=total_price,
+            guests=guests,
+            created_by=request.user
+
+        )
+
+        return JsonResponse({'success': True})
+    except Exception as e:
+        print('Error', e)
+
+        return JsonResponse({'success': False})
+
+
+@api_view(['POST'])
+def toggle_favorite(request, pk):
+    property = Property.objects.get(pk=pk)
+
+    if request.user in property.favorited.all():
+        property.favorited.remove(request.user)
+
+        return JsonResponse({'is_favorite': False})
+    else:
+        property.favorited.add(request.user)
+
+        return JsonResponse({'is_favorite': True})
